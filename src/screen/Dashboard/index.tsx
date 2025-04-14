7645022342c7eae5bbd547a319e4886452592f46
manishalgooo <manishsinghbais34766@gmail.com>
2025-04-14 17:45:13 +0500

    Please elevate the order of top priority to be sign-in, onboarding, general settings, webchat settings, dashboard (with just inbox and payments), inbox, and payments. That's like the whole mobile app at first.

--- src/screen/Dashboard/index.tsx
+++ src/screen/Dashboard/index.tsx
@@ -1,4 +1,11 @@
-import {ScrollView, Text, View} from 'react-native';
+import {
+  Image,
+  Modal,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import styles from './style';
 import Header from '../../component/common/Header';
 import {Icon} from '../../assest';
@@ -18,18 +25,39 @@ import DashBoardModal from '../../component/other/DashboardComponent/DashBoardMo
 import {useState} from 'react';
 import Input from '../../component/common/Input';
 import Buttons from '../../component/common/Buttons';
-import {COLOR} from '../../themes/StyleGuides';
+import {COLOR, commonStyles} from '../../themes/StyleGuides';
 import SCREEN from '../../data/ScrName';
 
+const data = [
+  {
+    title: 'Live Chat',
+    icons: Icon.MicroPhone,
+  },
+  {
+    title: 'Live Video',
+    icons: Icon.livecall,
+  },
+  {
+    title: 'Phone Call',
+    icons: Icon.PhoneCallsecond,
+  },
+  {
+    title: 'Email',
+    icons: Icon.Emailsecond,
+  },
+
+  {
+    title: 'Create Ticket',
+    icons: Icon.CreateTicket,
+  },
+];
 const Dashboard = (props: any) => {
   const {navigation} = props;
   const [openModal, setOpenModal] = useState(false);
   const [selector, setSelector] = useState(null);
 
-
   return (
     <View style={styles.container}>
-
       <Header
         firstIcon={Icon.bars}
         title="Dashboard"
@@ -140,8 +168,44 @@ const Dashboard = (props: any) => {
         />
       </ScrollView>
       <View style={styles.bottomBar}></View>
-      <DashBoardModal visible={false} onpress={() => setOpenModal(false)} selector={selector} getData={()=>setSelector} />
+      <Modal visible={openModal} transparent >
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: COLOR.lightBlack,
+            justifyContent: 'center',
+          }}>
+          <View style={styles.modelStyle}>
+            <TouchableOpacity
+              style={styles.crossBtn}
+              onPress={() => setOpenModal(false)}>
+              <Image source={Icon.crose} style={styles.cross} />
+            </TouchableOpacity>
 
+            <View>
+              {data.map((item, index: any) => (
+                <View
+                  key={index}
+                  style={{
+                    ...commonStyles.horizontalView,
+                    marginBottom: '4%',
+                    marginLeft: '5%',
+                  }}>
+                  <TouchableOpacity
+                    onPress={() => setSelector(index)}
+                    style={styles.selctor}>
+                    {selector === index && <View style={styles.selectorS} />}
+                  </TouchableOpacity>
+                  <View style={styles.titleContainer}>
+                    <Image style={styles.icon} source={item.icons} />
+                    <Text style={styles.title}>{item.title}</Text>
+                  </View>
+                </View>
+              ))}
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
--- src/screen/Dashboard/style.tsx
+++ src/screen/Dashboard/style.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import { COLOR, commonStyles } from '../../themes/StyleGuides';
+import { COLOR, commonStyles, normalize } from '../../themes/StyleGuides';
 
 const styles = StyleSheet.create({
  container: {
@@ -16,7 +16,36 @@ const styles = StyleSheet.create({
     bottom:0
   },
   BtnCOnatiner:{alignSelf:"center",width:"90%",marginTop:"5%",marginBottom:"20%"},
-
+ cross: {width: 16, height: 16, resizeMode: 'contain'},
+  crossBtn: {alignSelf: 'flex-end', marginRight: '5%', marginTop: '2%'},
+  modelStyle: {
+    width: '65%',
+    backgroundColor: COLOR.whites,
+    alignSelf: 'center',
+    borderRadius: 10,
+    paddingVertical: '5%',
+  },
+  titleContainer: {...commonStyles.horizontalView},
+  title: {
+    fontSize: normalize(18),
+    fontWeight: '700',
+    color: COLOR.gray_5,
+    marginLeft: '5%',
+  },
+  icon: {width: 20, height: 20, resizeMode: 'contain', marginLeft: '5%'},
+  selctor: {
+    width: 25,
+    height: 25,
+    backgroundColor: COLOR.gray_7,
+    ...commonStyles.center,
+    borderRadius: 15,
+  },
+  selectorS: {
+    width: 15,
+    height: 15,
+    borderRadius: 15,
+    backgroundColor: COLOR.perpal_3,
+  },
 
 });
 
